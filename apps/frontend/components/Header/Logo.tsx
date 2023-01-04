b02364e84ba8a350d2c03d62e03f7e97ccecceed
Manuel Schierenberg <mail@elanum.de>
2023-01-04 16:24:08 +0100

    feat: :sparkles: update footer & header

--- apps/frontend/components/Header/Logo.tsx
+++ apps/frontend/components/Header/Logo.tsx
@@ -1,18 +1,21 @@
 import classNames from 'classnames';
 import Image from 'next/image';
+import Link from 'next/link';
 import { BBECircle } from '../Images';
 
 const Logo = (): JSX.Element => {
   return (
-    <figure className={classNames('flex', 'items-center', 'px-4')}>
-      <Image src={BBECircle} alt="BBE" width={90} height={90} />
-      <figcaption
-        className={classNames('ml-2', 'flex', 'flex-col', 'text-2xl', 'sm:text-3xl', 'font-play', 'font-normal')}
-      >
-        <span>Berlin-Brandenburg</span>
-        <span>eSports</span>
-      </figcaption>
-    </figure>
+    <Link href="/">
+      <figure className={classNames('flex', 'items-center', 'px-4')}>
+        <Image src={BBECircle} alt="BBE" width={90} height={90} />
+        <figcaption
+          className={classNames('ml-2', 'flex', 'flex-col', 'text-2xl', 'sm:text-3xl', 'font-play', 'font-normal')}
+        >
+          <span>Berlin-Brandenburg</span>
+          <span>eSports</span>
+        </figcaption>
+      </figure>
+    </Link>
   );
 };
 
