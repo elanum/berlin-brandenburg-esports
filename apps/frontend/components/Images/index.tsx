7891bb4971ae77d033520101627c3a700c746a78
Manuel Schierenberg <mail@elanum.de>
2023-01-11 14:18:13 +0100

    feat: :bento: add images for board

--- apps/frontend/components/Images/index.tsx
+++ apps/frontend/components/Images/index.tsx
@@ -1,2 +1,5 @@
 export { default as BBECircle } from '../../public/images/bbe-circle.png';
-export { default as PlayerImage } from '../../public/images/player.jpg';
+export { default as ErinImage } from '../../public/images/erin.webp';
+export { default as JakobImage } from '../../public/images/jakob.webp';
+export { default as MarliesImage } from '../../public/images/marlies.webp';
+export { default as PlayerImage } from '../../public/images/player.webp';
